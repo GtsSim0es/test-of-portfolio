64f6829edecccc04bbf5682a44ef6dfe78dca686
Gabriel Simões <gabrielsimoest@gmail.com>
2022-06-24 15:23:02 -0300

    teste v1

---
+++ projeto_vue/src/components/lifeCycle.vue
@@ -0,0 +1,18 @@
+<template>
+<h1>Meu nome é: {{ nome }}</h1>
+</template>
+
+<script>
+
+export default {
+    name: 'lifeCycle',
+    data() {
+        return{
+            name: 'ainda não sei'
+        }
+    },
+    created() {
+        this.nome = 'Gabriel'
+    }
+}
+</script>
---
+++ projeto_vue/src/components/primeirocomponente.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>Olá Vue!</h1>
+</template>
