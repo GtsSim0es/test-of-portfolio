412b45a75bf8628829e66e473c3dad342d4b5bb7
Gabriel Simões <gabrielsimoest@gmail.com>
2022-06-27 14:53:08 -0300

    v1

--- projeto_vue/src/App.vue
+++ projeto_vue/src/App.vue
@@ -1,18 +1,26 @@
 <template>
-<div>
-  <primeirocomponente />
-</div>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import primeirocomponente from "./components/primeirocomponente.vue"
-import lifeCycle from "./components/lifeCycle.vue"
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-   name: "app",
-   components: {
-      primeirocomponente,
-      lifeCycle
-   }
+  name: 'App',
+  components: {
+    HelloWorld
+  }
 }
 </script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- projeto_vue/src/components/primeirocomponente.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    <h1>Olá Vue!</h1>
-</template>
